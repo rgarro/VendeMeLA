67dd0191bf9897247d7b80b61848dfd4113f4069
rgarro <rgarro@gmail.com>
2018-02-24 12:44:40 -0600

    on a bus in a psichedelic trip saying prayers ...
    on a bus in a psichedelic trip saying prayers ...

--- VendeMeLA/VendeMeLA/Form1.Designer.vb
+++ VendeMeLA/VendeMeLA/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.ClientSize = New System.Drawing.Size(569, 412)
         Me.Name = "Form1"
         Me.Text = "VendeMeLA"
         Me.ResumeLayout(False)
