4217db37fd24b64b885b638eb1ae4073afac883c
rgarro <rgarro@gmail.com>
2018-02-27 18:41:03 -0600

    disposing AndAlso components IsNot Nothing Then ...
     disposing AndAlso components IsNot Nothing Then ...

---
+++ VendeMeLA/VendeMeLA/EmbodegaloFRM.vb
@@ -0,0 +1,3 @@
+﻿Public Class EmbodegaloFRM
+
+End Class
--- VendeMeLA/VendeMeLA/Form1.vb
+++ VendeMeLA/VendeMeLA/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Public Class Form1
     Private Sub showVendeFormBtn_Click(sender As Object, e As EventArgs) Handles showVendeFormBtn.Click
         VendeloFRM.Show()
+    End Sub
 
+    Private Sub showEmbodegaFormBtn_Click(sender As Object, e As EventArgs) Handles showEmbodegaFormBtn.Click
+        EmbodegaloFRM.Show()
     End Sub
 End Class
