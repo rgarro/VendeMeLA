e4a6b6d4522f30f0fbf65aa5d38daec463bcc074
rgarro <rgarro@gmail.com>
2018-02-28 15:38:37 -0600

    visual basic es una chachara de nacos ...
    visual basic es una chachara de nacos ...

--- VendeMeLA/VendeMeLA/EmbodegaloFRM.vb
+++ VendeMeLA/VendeMeLA/EmbodegaloFRM.vb
@@ -1,8 +1,19 @@
 ﻿Public Class EmbodegaloFRM
 
     Private Function esValidoEmbodegable()
-        Dim isValid As Boolean
-        isValid = True
+        Dim isValid As Boolean = True
+        Dim Errors As New List(Of String)
+        'Etiqueta del producto
+        If (etiquetaComercialTXT.Text.Length < 1) Then
+            Errors.Add("La Etiqueta es requerida")
+        End If
+        'Precio del producto
+        If (Not IsNumeric(precioUnidadTXT.Text)) Then
+            Errors.Add("El Precion es requerido y debe ser un numero")
+        End If
+        If Errors.Count > 0 Then
+            isValid = False
+        End If
         Return isValid
     End Function
 
